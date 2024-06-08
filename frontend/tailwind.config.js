9509dd5e22af0fd81831132bb9798fa2b52bb80c
Muhammed Noushad <mammumuhammed606@gmail.com>
2024-06-08 11:53:41 +0530

    Dark mode turned of

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'roboto-condensed': ['Roboto Condensed', 'sans-serif'],
+        "roboto-condensed": ["Roboto Condensed", "sans-serif"],
       },
     },
   },
   plugins: [
     // eslint-disable-next-line no-undef
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/aspect-ratio"),
     // eslint-disable-next-line no-undef
     require("daisyui"),
     // eslint-disable-next-line no-undef
     require("tailwind-scrollbar-hide"), // Adding the scrollbar-hide plugin
   ],
-}
+  darkMode: false,
+};
